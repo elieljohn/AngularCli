efa2ea3144bd3b6fddb5b0410d2c03d34ec53529
elieljohn <ejmalabar88@gmail.com>
2024-04-06 15:29:46 +0100

    Added routing modules

--- angular-routing/src/app/admin/admin.module.ts
+++ angular-routing/src/app/admin/admin.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { EmailBlastComponent } from './email-blast/email-blast.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+    EmailBlastComponent,
+    UsersComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
---
+++ angular-routing/src/app/admin/email-blast/email-blast.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-blast',
+  templateUrl: './email-blast.component.html',
+  styleUrl: './email-blast.component.css'
+})
+export class EmailBlastComponent {
+
+}
