9188e9ab40e91878e498d6c1173f240721bd66c7
elieljohn <ejmalabar88@gmail.com>
2024-04-07 11:40:51 +0100

    Added angular material navigation schematics

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -7,17 +7,29 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerComponent } from './customer/customer.component';
 import { AdminModule } from './admin/admin.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    CustomerComponent
+    CustomerComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminModule
+    AdminModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     provideClientHydration(),
