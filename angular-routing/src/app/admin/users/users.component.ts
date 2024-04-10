9d3fa59e144d615706584167e7119dc87f045d75
elieljohn <ejmalabar88@gmail.com>
2024-04-10 14:22:40 +0100

    Run 'ng test'

--- angular-routing/src/app/admin/users/users.component.ts
+++ angular-routing/src/app/admin/users/users.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+  users: any[] = [];
+  hasPermission: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers()
+      .then(users => this.users = users)
+      .catch(e => console.log(e.message));
+    } else {
+      this.users = []
+    }
+  }
+
+  async getUsers() {
+    return [
+      {name: 'john', email: 'john@angular.com'},
+      {name: 'colleen', email: 'colleen@angular.com'}
+    ]
+  }
 
 }
